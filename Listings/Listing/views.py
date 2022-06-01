60cfb20242e129c9a29106530dd89eae4afe858a
JacobItopa <sanijacobs@gmail.com>
2022-06-01 11:57:17 +0100

    Second commit

--- Listings/Listing/views.py
+++ Listings/Listing/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Listing
+#from django.views import generic
 
 # Create your views here.
+
+def listing_list(request):
+    listings=Listing.objects.all()
+    context = {
+        'listings':listings
+    }
+    return render(request, 'listing/listings.html', context)
+
+def listing_retrive(request, pk):
+    listing = Listing.objects.get(id=pk)
+    context = {
+        'listing':listing
+    }
+    return render(request, 'listing/retrieve.html', context)
