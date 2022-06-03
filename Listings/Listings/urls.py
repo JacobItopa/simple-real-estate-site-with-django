35eb44194752b52a9477ded3a6aa3a01eb9bfed5
JacobItopa <sanijacobs@gmail.com>
2022-06-03 10:35:15 +0100

    fourth commit

--- Listings/Listings/urls.py
+++ Listings/Listings/urls.py
@@ -1,11 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
-from Listing.views import listing_list, listing_retrive, listing_create
+from Listing.views import listing_list, listing_retrive, listing_create, listing_update, listing_delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', listing_list),
     path('listings/<int:pk>/', listing_retrive),
+    path('listings/<int:pk>/update', listing_update),
+    path('listings/<int:pk>/delete', listing_delete),
     path('create/', listing_create),
+    
 ]
